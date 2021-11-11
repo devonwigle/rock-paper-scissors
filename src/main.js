bb52737dcbd1ec890553bf5ec5c08c3113dd8093
Devon Wigle <devonwigle@gmail.com>
2021-11-11 15:19:08 -0600

    Add showDifficultGame function and variables

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 //variables
 var difficultySelectorView = document.querySelector('.difficulty-selector-view');
 var classicGameView = document.querySelector('.classic-game-view');
+var difficultGameView = document.querySelector('.difficult-game-view')
+var difficultDifficultyButton = document.querySelector('.difficult-difficulty-selector');
+var classicDifficultyButton = document.querySelector('.classic-difficulty-selector');
+var difficultGameView = document.querySelector('.difficult-game-view');
 //eventListeners
-difficultySelectorView.addEventListener('click', showClassicGame);
+classicDifficultyButton.addEventListener('click', showClassicGame);
+difficultDifficultyButton.addEventListener('click', showDifficultGame);
 //functions
 function showClassicGame() {
   addClass(difficultySelectorView, 'hidden');
   removeClass(classicGameView, 'hidden');
 };
+
+function showDifficultGame() {
+  addClass(difficultySelectorView, 'hidden');
+  removeClass(classicGameView, 'hidden');
+  removeClass(difficultGameView, 'hidden');
+}
 
 
 function removeClass(element, rule) {
