5df58cded90c048de5c5ff6a6ab3523af2e0ad11
Devon Wigle <devonwigle@gmail.com>
2021-11-12 11:29:09 -0600

    Add eventlisteners to fighter buttons

--- src/main.js
+++ src/main.js
@@ -2,19 +2,30 @@
 //views
 var difficultySelectorView = document.querySelector('.difficulty-selector-view');
 var classicGameView = document.querySelector('.classic-game-view');
-var difficultGameView = document.querySelector('.difficult-game-view')
 var difficultGameView = document.querySelector('.difficult-game-view');
+var outcomeGameView = document.querySelector('.outcome-game-view')
 //buttons
 var difficultDifficultyButton = document.querySelector('.difficult-difficulty-selector');
 var classicDifficultyButton = document.querySelector('.classic-difficulty-selector');
 var changeDifficulty = document.querySelector('.change-difficulty')
+var childrenButton = document.querySelector('#children');
+var parentsButton = document.querySelector('#parents');
+var grandparentsButton = document.querySelector('#grandparents');
+var catButton = document.querySelector('#cat');
+var dogButton = document.querySelector('#dog');
+
 // players
-var computer = new Player('Computer', './assets/computer.svg')
-var human = new Player('Human', './assets/astronaut.svg')
+var computer = {};
+var human = {};
 //eventListeners
 classicDifficultyButton.addEventListener('click', showClassicGame);
 difficultDifficultyButton.addEventListener('click', showDifficultGame);
-changeDifficulty.addEventListener('click', showDifficultySelector)
+changeDifficulty.addEventListener('click', showDifficultySelector);
+childrenButton.addEventListener('click', showOutcome);
+parentsButton.addEventListener('click', showOutcome);
+grandparentsButton.addEventListener('click', showOutcome);
+catButton.addEventListener('click', showOutcome);
+dogButton.addEventListener('click', showOutcome);
 //functions
 function showClassicGame() {
   addClass(difficultySelectorView, 'hidden');
@@ -29,6 +40,12 @@ function showDifficultGame() {
 
 function showDifficultySelector() {
   removeClass(difficultySelectorView, 'hidden');
+  addClass(classicGameView, 'hidden');
+  addClass(difficultGameView, 'hidden');
+}
+
+function showOutcome() {
+  removeClass(outcomeGameView, 'hidden');
   addClass(classicGameView, 'hidden');
   addClass(difficultGameView, 'hidden');
 }
