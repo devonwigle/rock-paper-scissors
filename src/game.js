ac78e45de59ccbb05b37e82486fe78198159fbb7
Devon Wigle <devonwigle@gmail.com>
2021-11-12 18:36:44 -0600

    Add choices array and play method to push elements to array

--- src/game.js
+++ src/game.js
@@ -1,19 +1,33 @@
 class Game {
-  constructor(player, difficultyLevel) {
-    this.player = player;
+  constructor(difficultyLevel) {
+    this.player1 = new Player('Human', './assets/astronaut.svg');
+    this.player2 = new Player('Computer', '.assets/computer.svg')
     this.type = difficultyLevel;
+    this.choices = [];
+    this.winner = null;
   }
+
+  play() {
+    if (this.type = 'classic') {
+      this.choices.push('children', 'parents', 'grandparents')
+    }
+    this.choices.push('children', 'parents', 'grandparents', 'dog', 'cat')
+  }
+  
   trackData() {
 
   }
-  winConditions() {
-    if(children.clicked && !parents.clicked) {
+  winConditions(humanChoice, computerChoice) {
+    if(children.clicked && !parents.clicked && !dog.clicked) {
 
-    } else if (parents.clicked && !grandparents.clicked) {
+    } else if (parents.clicked && !grandparents.clicked && !cat.clicked) {
 
-    } else if (grandparents.clicked && !children.clicked) {
+    } else if (grandparents.clicked && !children.clicked && !cat.clicked) {
 
-    } else if ()// ties
+    } else if (cat.clicked && !dog.clicked && !children.clicked) {
+
+    } else if (dog.clicked && !parents.clicked && !grandparents.clicked)
+  // ties
 
   }
   timeOut() {
