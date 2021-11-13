ea19aabd363362f4d98531b3ae065f18502257e1
Devon Wigle <devonwigle@gmail.com>
2021-11-13 11:32:36 -0600

    Add returns in winConditions

--- src/game.js
+++ src/game.js
@@ -20,24 +20,31 @@ class Game {
   winConditions(humanChoice, computerChoice) {
     if (player1.choice === player2.choice) {
       this.winner = 'Tie!'
+      return 'Tie!'
     } else if (player1.choice === 'children' && (player2.choice !== 'parents' || player2.choice !== 'dog')) {
       this.winner = player1.name
       player1.wins ++
+      return `${game.player1.name} wins!`
     } else if (player1.choice === 'parents' && (player2.choice !== 'grandparents' ||  player2.choice !== 'cat')) {
       this.winner = player1.name
       player1.wins ++
+      return `${game.player1.name} wins!`
     } else if (player1.choice === 'grandparents' && (player2.choice !== 'children' || player2.choice !== 'cat')) {
       this.winner = player1.name
       player1.wins ++
+      return `${game.player1.name} wins!`
     } else if (player1.choice === 'cat' && (player2.choice !== 'dog' || player2.choice !== 'children')) {
       this.winner = player1.name
       player1.wins ++
+      return `${game.player1.name} wins!`
     } else if (player1.choice === 'dog' && (player2.choice !== 'parents' || player2.choice !== 'grandparents')) {
       this.winner = player1.name
       player1.wins ++
+      return `${game.player1.name} wins!`
     }
     this.winner = player2.name
     player2.wins ++
+    return `${game.player2.name} wins!`
 
   }
   timeOut() {
