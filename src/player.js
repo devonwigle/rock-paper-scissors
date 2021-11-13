79867a24979aff7626f3ce998861af42aa488757
Devon Wigle <devonwigle@gmail.com>
2021-11-13 14:32:21 -0600

    Fix takeTurn method to work

--- src/player.js
+++ src/player.js
@@ -3,7 +3,7 @@ class Player {
     this.name = name;
     this.token = token;
     this.wins = wins || 0;
-    this.choice = null;
+    this.choice = '';
   }
   saveWinsToStorage() {
 
@@ -12,10 +12,11 @@ class Player {
 
   }
   takeTurn(choice) {
-    if (!choice) {
-      var chosenIndex = Math.floor(Math.random() * game.choices.length)
+    if (choice) {
+      this.choice = choice
+    } else if (!choice) {
+      var chosenIndex = Math.floor(Math.random() * game.choices.length);
       this.choice = game.choices[chosenIndex]
     }
-    this.choice = choice;
   }
 }
